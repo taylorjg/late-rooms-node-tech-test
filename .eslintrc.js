32e128d3ec9810745c917ff389dc45609d62a3a2
Jonathan Taylor <jonathan.taylor.programmer@gmail.com>
2019-04-15 09:48:08 +0100

    Add eslint, mocha, chai etc.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: "eslint:recommended",
+  env: {
+    es6: true,
+    node: true,
+    mocha: true
+  },
+  globals: {
+    chai: "readonly"
+  },
+  parserOptions: {
+    ecmaVersion: 2018
+  }
+}
